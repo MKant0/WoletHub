5f4623b23951f7d88c616787d66b8d40748b0804
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-06-24 16:33:30 -0400

    autenticacion

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,6 @@
 class BankAccount < ApplicationRecord
   belongs_to :user
+  has_many :banks, dependent: :destroy
+
+  validates :name, :email, :password, :bank, :prefered_currency, presence: true
 end
