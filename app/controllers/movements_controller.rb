968c42d9a1d172581b042bb61e0680ad983e2125
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-07-01 14:05:57 -0400

    pequenio avance

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,5 +1,15 @@
 class MovementsController < ApplicationController
 
+  def new
+    @movements = Movement.new
+  end
+
+  def create
+  end
+
+  def movement_confirmation
+  end
+
   def index_movements
     @movements = FintocService.get_movements(account_id, link_token, api_key)
     # Ahora puedes usar @movements en tu vista para mostrar los movimientos de la cuenta
