bc2b1b7dc46d25b44b52c4963012048a48ebb433
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-07-13 21:14:39 -0400

    done

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
-require "rails/all"
+require 'rails/all'
+require 'dotenv/load' if ['development', 'test'].include? ENV['RAILS_ENV']
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
