750316913c3611cdea735c3a5044a8f871c09b6c
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-07-15 17:49:15 -0400

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -8,47 +8,45 @@ class WebhookController < ApplicationController
   def data_fintoc
     payload = request.body.read
     event = nil
-    puts  params[:data][:link_token]
-    FintocAccount.create(widget_token: link_token)
+    link_token = params[:data][:link_token]
+    puts link_token
     begin
+      # Parse JSON payload
+      event = JSON.parse(payload)
     rescue JSON::ParserError => e
       # Invalid payload
       render json: { error: 'Invalid payload' }, status: 400
       return
     end
-     seen_event = WebhookEvent.find_by(fintoc_event_id: event['id'])
-     if seen_event
-       render json: { message: 'Event already processed' }, status: 200
-       return
-     end
+
+    seen_event = WebhookEvent.find_by(fintoc_event_id: event['id'])
+    if seen_event
+      render json: { message: 'Event already processed' }, status: 200
+      return
+    end
 
     # save new event for idempotency
     new_event = WebhookEvent.create!(
       fintoc_event_id: event['id'],
-       #type: event['type'],
-       #data: event['data']
     )
+
     # Handle the event
     case event['type']
     when 'link.created'
-       link_token = payload[:data][:link_token]
-      # puts "Link token: #{link_token}"
-      # name = new_event['data']['holder_name'] # Assuming the name is in the 'holder_name' field
-       FintocAccount.create(widget_token: link_token)
-      # # bank_account_id = session[:bank_account_id] # Make sure to set this in your application properly
-
+      if link_token
+        FintocAccount.create(widget_token: link_token)
+      else
+        puts "No link_token in the event"
+      end
     when 'link.credentials_changed'
       link_id = event['data']['id']
       # Then define and call a method to handle the event.
-
     when 'link.refresh_intent.succeeded'
       link_id = event['data']['refreshed_object_id']
       # Then define and call a method to handle the link refreshed event.
-
     when 'account.refresh_intent.succeeded'
       account_id = event['data']['refreshed_object_id']
       # Then define and call a method to handle the account refreshed event.
-
     else
       # Unexpected event type
       puts "Unhandled event type: #{event['type']}"
