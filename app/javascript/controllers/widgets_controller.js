da5ccf84b9145bbe159949ee893aa201514bae74
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-07-15 10:15:43 -0400

    ruta de webhook corregida

--- app/javascript/controllers/widgets_controller.js
+++ app/javascript/controllers/widgets_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   startFintocProcess(event) {
     let BankAccountId = event.target.dataset.bankAccountId;
     sessionStorage.setItem('bankAccountId', BankAccountId);
-  } 
+  }
   connect() {
     console.log("Hello, Stimulus!", this.element);
     window.onload = () => {
@@ -13,7 +13,7 @@ export default class extends Controller {
         publicKey: 'pk_test_xwUGiHD3-zsftKEioAvCGjAk6qQrTS65',
         holderType: 'individual',
         product: 'movements',
-        webhookUrl: 'https://webhook.site/7c9c93d1-a80b-4886-8fa8-145c4215fb86',
+        webhookUrl: 'https://wolethab-914-db0606c8d9c5.herokuapp.com/webhook',
         onSuccess: function(response) {
           fetch("/links", {
             method: "POST",
