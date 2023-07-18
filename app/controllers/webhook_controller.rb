88463ca9c3de3cfe060d0f7acbb71f3aa2915307
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-07-18 19:37:14 -0400

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -8,36 +8,26 @@ class WebhookController < ApplicationController
   def data_fintoc
     data = JSON.parse(request.body.read)
     link_id = data["data"]["id"]
-    token = FintocServices.link_token(link_id)
+    link_token = data["data"]["link_token"] # obteniendo el link_token directamente de los datos del webhook
 
-    if token == "wrong_link_id"
-      render json: { error: "El link id es incorrecto." }, status: 400
-      return
-    end
-
-    holder_id = data["data"]["holder_id"]
-    holder_name = data["data"]["holder_name"]
-    accounts = data["data"]["accounts"]
-
-    accounts.each do |account|
-      fintoc_account = FintocAccount.create(
-        holder_id: holder_id,
-        holder_name: holder_name,
-        fintoc_token: token,
-        name: account["name"],
-        currency: account["currency"],
-        number: account["number"],
-        account_type: account["type"],
-        official_name: account["official_name"],
-        refreshed_at: account["refreshed_at"]
-      )
-      unless fintoc_account.persisted?
-        render json: { error: "No se pudo crear la cuenta Fintoc." }, status: 400
-        return
+    if data["type"] == "link.created"
+      link_id = data["data"]["id"]
+      accounts = data["data"]["accounts"]
+      accounts.each do |account|
+        FintocAccount.create(
+          name: account["name"],
+          number: account["number"],
+          account_type: account["type"],
+          widget_token: link_token,
+          official_name: account["official_name"],
+          holder_id: account["holder_id"],
+          holder_name: account["holder_name"],
+          refreshed_at: account["refreshed_at"]
+        )
       end
-      puts FintocAccount.last
+      render json: { message: "account created successfully" }, status: :ok
+    else
+      render json: { message: "invalid data" }, status: :bad_request
     end
-
-    render json: {}, status: 200
   end
 end
