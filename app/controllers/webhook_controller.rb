5aed01a776e2a97fdd15c9d394d2a2d274c55468
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-07-18 20:00:35 -0400

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,5 @@
 class WebhookController < ApplicationController
+
   skip_before_action :verify_authenticity_token, only: [:data_fintoc]
   skip_before_action :authenticate_user!, only: [:data_fintoc]
 
@@ -7,10 +8,7 @@ class WebhookController < ApplicationController
   def data_fintoc
     payload = request.body.read
     event = nil
-    link_token = params.dig(:data, :link_token)
-    puts link_token
-    fintoc_account = FintocAccount.create(widget_token: link_token)
-    puts fintoc_account.inspect
+    link_token = params[:data][:link_token]
 
     begin
       # Parse JSON payload
@@ -27,28 +25,29 @@ class WebhookController < ApplicationController
       return
     end
 
-    # Save new event for idempotency
-    new_event = WebhookEvent.create!(fintoc_event_id: event['id'])
-
+    # save new event for idempotency
+    new_event = WebhookEvent.create!(
+      fintoc_event_id: event['id'],
+    )
     # Handle the event
     case event['type']
     when 'link.created'
-      if link_token.present?
+      if link_token
         if fintoc_account.errors.any?
           puts fintoc_account.errors.full_messages
         end
       else
-        puts 'No link_token in the event'
+        puts "No link_token in the event"
       end
     when 'link.credentials_changed'
-      link_id = event.dig('data', 'id')
-      # Define and call a method to handle the event.
+      link_id = event['data']['id']
+      # Then define and call a method to handle the event.
     when 'link.refresh_intent.succeeded'
-      link_id = event.dig('data', 'refreshed_object_id')
-      # Define and call a method to handle the link refreshed event.
+      link_id = event['data']['refreshed_object_id']
+      # Then define and call a method to handle the link refreshed event.
     when 'account.refresh_intent.succeeded'
-      account_id = event.dig('data', 'refreshed_object_id')
-      # Define and call a method to handle the account refreshed event.
+      account_id = event['data']['refreshed_object_id']
+      # Then define and call a method to handle the account refreshed event.
     else
       # Unexpected event type
       puts "Unhandled event type: #{event['type']}"
@@ -56,30 +55,4 @@ class WebhookController < ApplicationController
 
     render json: { message: 'Webhook received' }, status: 200
   end
-
-  # Crea un hash para almacenar los datos requeridos
-data = {
-  links_created: 0,
-  refresh_intent_succeeded: 0,
-  accounts_refreshed: 0,
-  new_movements: 0
-}
-
-# Itera sobre las líneas del log
-log.each_line do |line|
-  if line.include?("link.created")
-    data[:links_created] += 1
-  elsif line.include?("account.refresh_intent.succeeded")
-    data[:refresh_intent_succeeded] += 1
-    data[:accounts_refreshed] += 1
-    match = line.match(/"new_movements"=>(\d+)/)
-    data[:new_movements] += match[1].to_i if match
-  end
-end
-
-puts "Número de links creados: #{data[:links_created]}"
-puts "Número de refresh_intent_succeeded: #{data[:refresh_intent_succeeded]}"
-puts "Número de cuentas actualizadas: #{data[:accounts_refreshed]}"
-puts "Total de nuevos movimientos: #{data[:new_movements]}"
-
 end
